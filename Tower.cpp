36953e2b63e1858f2d3aab5c37a2c15e856bc653
Ian Tharp <64619437+Ian-Tharp@users.noreply.github.com>
2021-10-28 12:16:27 -0500

    Updated Tower.cpp

--- Tower.cpp
+++ Tower.cpp
@@ -15,12 +15,26 @@
 
 CTower::CTower(const Vector2& p) : CObject(eSprite::Turret, p) {
 	m_bStatic = true; //towers are static
-	m_fRotSpeed = 0;
-}
+} //constructor
 
 //Look towards the enemy that passes through the radius of the tower
 void CTower::RotateTowards(const Vector2& position) {
-	//do something here
+	const Vector2 v = position - m_vPos; //vector from target to player
+	const float theta = atan2f(v.y, v.x); //orientation of that vector
+	float diff = m_fRoll - theta;
+	NormalizeAngle(diff); //normalizing to [-pi, pi]
+
+	const float fAngleDelta = 0.05f; //allowable angle discrepancy
+	const float fTrackingSpeed = 2.0f; //rotation speed when tracking
+
+	if (diff > fAngleDelta)m_fRotSpeed = -fTrackingSpeed; //clockwise
+	else if (diff < -fAngleDelta)m_fRotSpeed = fTrackingSpeed; //counterclockwsise
+	else m_fRotSpeed = 0; //stop rotating
+
+	if (fabsf(diff) < fAngleDelta && m_pGunFireEvent->Triggered()) {
+		m_pObjectManager->FireGun(this, eSprite::Bullet2);
+	} //Rotate Towards target
+
 }
 
 void CTower::move() {
