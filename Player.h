8bb70ed6b90303058ce18f26a4d73a3de40d379f
Ian Tharp <64619437+Ian-Tharp@users.noreply.github.com>
2021-10-28 12:15:47 -0500

    Updated Player.h

--- Player.h
+++ Player.h
@@ -5,7 +5,8 @@
 #define __L4RC_GAME_PLAYER_H__
 
 #include "Object.h"
-#include <string>
+#include "mapGen.h"
+#include "TileManager.h"
 
 /// \brief The player object. 
 ///
@@ -16,24 +17,51 @@
 
 class CPlayer: public CObject{
   protected:  
-	  const UINT m_nMaxCurrency = 1000; ///< Maximum amount of currency the player may hold at one time
+	  const INT MaxCurrency = 1000; ///< Maximum amount of currency the player may hold at one time
 	  int CurrentCurrency = 50; ///< Current currency the player has to purchase towers. Starts off with 50
-	  int Lives = 100; ///< Amount of lives that the player has to survive
-	  const char pchar = '30';
+	  int CurrentLives = 100; ///< Amount of lives that the player has to survive
+	  const INT MaxLives = 100; ///< Maximum amount of lives the player can have at one time
+	  bool Right = false;
+	  bool Left = false;
+	  bool Up = false;
+	  bool Down = false;
+
+	  int xPos = 0;
+	  int yPos = 0;
 
   public:
     CPlayer(const Vector2& p); ///< Constructor.
 
     virtual void move(); ///< Move player object.
-	virtual void draw(); ///< Draw 
 
+	//Player interaction to place towers
+	void PlaceTower(int Tower);
+
+	//Player movement across grid tiles
+	void MoveRight();
+	void StopRight();
+	void MoveLeft();
+	void StopLeft();
+	void MoveUp();
+	void StopUp();
+	void MoveDown();
+	void StopDown();
+
+	//Adding & subtracting member variables
 	void RemoveLives(int Amount);
 	void RemoveCurrency(int Amount);
+	void AddLives(int Amount);
 	void AddCurrency(int Amount);
     
 	std::string GetCurrency();
+	std::string GetLives();
 
-    const Vector2& GetPos() const; ///< Get position.
+    const Vector2& GetPos() const; ///< Get position of sprite
+	std::string GetPlayerPosition(); ///< Get position of player sprite
+
+	//Get functions for player position on map
+	int GetXPos();
+	int GetYPos();
 }; //CPlayer
 
 #endif //__L4RC_GAME_PLAYER_H__
