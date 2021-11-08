40b125ac6b583e36b8a2641ee65dfeba84ebc48a
Ian Tharp <64619437+Ian-Tharp@users.noreply.github.com>
2021-11-08 17:53:46 -0600

    Updated Tower.cpp

--- Tower.cpp
+++ Tower.cpp
@@ -17,6 +17,21 @@ CTower::CTower(const Vector2& p) : CObject(eSprite::Turret, p) {
 	m_bStatic = true; //towers are static
 } //constructor
 
+CTower::CTower(int i, int j, Vector2& p) : CObject(eSprite::Turret, p) {
+	yPos = j;
+	xPos = i;
+	//Vector2 location(yPos, xPos);
+	m_bStatic = true;
+	m_vPos = Vector2((xPos + 0.5f) * ((size_t)m_pRenderer->GetWidth(eSprite::BlueTile) * 0.5f),
+		(8 - yPos + 0.5f) * ((size_t)m_pRenderer->GetWidth(eSprite::BlueTile) * 0.5f));
+}
+
+void CTower::move() {
+	if (m_pPlayer) {
+		const float radius = ((CTower*)m_pPlayer)->m_fRadius;
+	}
+}
+
 //Look towards the enemy that passes through the radius of the tower
 void CTower::RotateTowards(const Vector2& position) {
 	const Vector2 v = position - m_vPos; //vector from target to player
@@ -34,13 +49,24 @@ void CTower::RotateTowards(const Vector2& position) {
 	if (fabsf(diff) < fAngleDelta && m_pGunFireEvent->Triggered()) {
 		m_pObjectManager->FireGun(this, eSprite::Bullet2);
 	} //Rotate Towards target
-
 }
 
-void CTower::move() {
-
+const Vector2& CTower::GetPosition(int x, int y) {
+	return Vector2(x, y);
 }
 
-void CTower::draw() {
+int CTower::GetXPos() {
+	return xPos;
+}
 
+int CTower::GetYPos() {
+	return yPos;
+}
+
+void CTower::SetXPos(int x) {
+	xPos = x;
+}
+
+void CTower::SetYPos(int y) {
+	yPos = y;
 }
